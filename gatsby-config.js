cfbb9c8b976d713608596015b7766940b4da3fa3
Bhushan <bhushan61399@gmail.com>
2023-06-03 15:54:43 +0530

    profile information changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Bhushan kumar',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Bhushan Kumar is a software engineer who specializes in building exceptional Magento 2 eCommerce websites.',
+    siteUrl: 'https://bhushan-cs.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'BhushanKumar',
+        short_name: 'BhushanKumar',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
